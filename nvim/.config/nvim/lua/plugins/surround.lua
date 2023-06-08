8c8a20bfc233239b7ad9e8d109efc8bba1808cf4
Eric Unger <mr.eric.unger@gmail.com>
2023-06-08 10:14:24 +0100

    nvim: add nvim-surround plugin
    [nvim-surround](https://github.com/kylechui/nvim-surround)

---
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,10 @@
+return {
+  "kylechui/nvim-surround",
+  version = "*",
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      -- Add config or use default
+    })
+  end,
+}
