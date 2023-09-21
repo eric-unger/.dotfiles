d490ea4c2bb825b650d5f6f5c8f65dd84188fff3
Eric Unger <mr.eric.unger@gmail.com>
2023-09-21 08:40:16 +0000

    install: add additional packages to install

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,7 @@ mkdir -p ~/personal
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y git stow coreutils curl ripgrep luarocks postgresql postgresql-contrib libpq-dev
+sudo apt install -y git stow coreutils curl ripgrep luarocks postgresql postgresql-contrib libpq-dev \
+	libssl-dev zlib1g-dev libbz2-dev libreadline-dev \libsqlite3-dev wget curl llvm \
+	libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev fzf
 source "$HOME/.dotfiles/install/core.sh"
