e1e0401e2b6c79f1acdee730696e79eae5ed314b
Eric Unger <mr.eric.unger@gmail.com>
2023-09-21 08:39:52 +0000

    nvim: Add poetry python as the host

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -5,6 +5,7 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.g.python3_host_prog = "/home/eric/.asdf/shims/python"
 
 require("lazy").setup({
   spec = {
