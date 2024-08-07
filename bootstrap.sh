e153d458aef3b82be57737947034f3fa2e532870
Eric Unger <mr.eric.unger@gmail.com>
2024-08-07 11:47:45 +0100

    install: add starship

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,4 +10,7 @@ sudo apt upgrade -y
 sudo apt install -y git stow coreutils curl ripgrep luarocks postgresql postgresql-contrib libpq-dev \
   libssl-dev zlib1g-dev libbz2-dev libreadline-dev \libsqlite3-dev wget curl llvm \
   libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev fzf bat fd-find
+
+curl -sS https://starship.rs/install.sh | sh
+
 source "$HOME/.dotfiles/install/core.sh"
