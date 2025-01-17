0e868bab343c06768328c0a402c795f75d761719
Eric Unger <mr.eric.unger@gmail.com>
2025-01-17 12:10:31 +0000

    nvim back to defaulit colours

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -1,9 +1,11 @@
 -- Extra plugins
+--
 
 return {
   {
     "projekt0n/github-nvim-theme",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    enabled = false,
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("github-theme").setup({
