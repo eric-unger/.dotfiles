e8b1dbfc4f00b77006265a72f49e790416a52859
Eric Unger <mr.eric.unger@gmail.com>
2025-06-19 22:22:23 +0100

    Install: remove duplicated curl from the list

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,15 @@ mkdir -p ~/personal
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y git stow coreutils curl ripgrep luarocks postgresql postgresql-contrib libpq-dev \
-  libssl-dev zlib1g-dev libbz2-dev libreadline-dev \libsqlite3-dev wget curl llvm \
+sudo apt install -y git stow coreutils ripgrep luarocks postgresql postgresql-contrib libpq-dev \
+  libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
   libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev fzf fd-find
 
+# Install golang
+
+# Install asdf
+
+# Install starship
 curl -sS https://starship.rs/install.sh | sh
 
 source "$HOME/.dotfiles/install/core.sh"
